4948c8a7927deb84daca51a5873c5a5e81e797cc
priyanka <pandharbalepr1997@gmail.com>
2024-05-18 11:33:29 +0530

    added react projects

--- index.js
+++ index.js
@@ -34,14 +34,19 @@ function scrollToNextPage() {
 }
 
 // resume
-document
-  .getElementById("resume-link")
-  .addEventListener("click", function (event) {
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the anchor tag with class "nav-link" for the resume
+  const resumeLink = document.querySelector('.nav-links a[href*="resume"]');
+
+  // Add click event listener to the resume anchor tag
+  resumeLink.addEventListener("click", function (event) {
     var confirmDownload = confirm("Do you want to download my resume?");
     if (!confirmDownload) {
       event.preventDefault(); // Prevent default action (i.e., following the link)
     }
   });
+});
+
 
 document.addEventListener("DOMContentLoaded", function () {
   // Get the height of the content
@@ -73,3 +78,30 @@ function handleScroll() {
     }
   });
 }
+document.addEventListener('DOMContentLoaded', () => {
+  const filterButtons = document.querySelectorAll('.filter-btn');
+  const cards = document.querySelectorAll('.card');
+
+  filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      console.log('Button clicked:', button.textContent);
+      const category = button.getAttribute('data-category');
+      console.log('Category:', category);
+      filterProjects(category);
+    });
+  });
+
+  function filterProjects(category) {
+    console.log('Filtering projects by category:', category);
+    cards.forEach(card => {
+      if (category === 'all' || card.getAttribute('data-category') === category) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  }
+
+  // Initially display all projects
+  filterProjects('all');
+});
